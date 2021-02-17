7871ba2cd1c2965a3943a82a56f13c129d470ae1
Nahii <nahomayele7512@gmail.com>
2021-02-17 17:15:30 +0300

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://admin-nahom:123456Nahiaye@cluster0.hkgsq.mongodb.net/todolistDB", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("mongodb+srv://MYUSERNAME:MYPASSWORD@cluster0.hkgsq.mongodb.net/todolistDB", { useNewUrlParser: true, useUnifiedTopology: true });
 
 const itemsSchema = {
     name: String
